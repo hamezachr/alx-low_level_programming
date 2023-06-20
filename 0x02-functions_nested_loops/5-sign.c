7c6ed63dd6cb10fb31d017a8f381fa3d3c7462c3
hamezachr <hamzachr88@gmail.com>
2023-06-20 04:40:42 -0700

    tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the number to check
+ *
  * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
  * and prints the corresponding sign character to the output
  */
