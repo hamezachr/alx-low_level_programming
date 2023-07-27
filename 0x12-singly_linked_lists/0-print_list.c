4319afa19cd15572c46e598f267694a7dabbec72
hamezachr <hamezachr>
2023-07-27 04:53:54 -0700

    tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
@@ -10,7 +9,6 @@
 size_t print_list(const list_t *h)
 {
  size_t s = 0;
-
  while (h)
  {
  if (!h->str)
@@ -20,6 +18,5 @@ size_t print_list(const list_t *h)
  h = h->next;
  s++;
  }
-
  return (s);
 }
